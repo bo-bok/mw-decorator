dfe196c302bb185a457ac0254f10c0e512364859
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-18 14:38:39 +0000

    set up header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <title><?php wp_title(); ?></title>
+
+    <?php wp_head(); ?>
+  </head>
+
+
+  <body <?php body_class(); ?>>
+
+    <?php include_once("analyticstracking.php") ?>
+    <!-- call GA - add GA JS file-->
+
+    <div class="page-container">
+
+
+    <!--  SITE HEADER -->
+    <!-- ================================================================ -->
+    <header>
+    </header>
