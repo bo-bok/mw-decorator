eb8dc3cfcd6b78f0d5a3ca4f3c78eb68a34a49ed
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-18 19:34:03 +0000

    set up section pages. They are integrated into WP

---
+++ page-contact.php
@@ -0,0 +1,17 @@
+<?php
+  /*
+    Template Name: Contact Page
+  */
+get_header(); ?>
+
+
+<div class="contact-test">
+  <h1>contact page</h1>
+
+  Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+  Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna.
+
+</div>
+
+<?php get_footer(); ?>
