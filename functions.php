7c2319991bf25be4d17b02a638554196453db84c
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-18 14:37:26 +0000

    add theme support for menus and title tag

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+add_theme_support( 'menus' );
+add_theme_support( 'title-tag' );
+
+add_theme_support();
 // this function loads the style (css) files
 function mw_theme_styles(){
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
