4c3a09321fec7750df93edd87f8e6125ca5263ef
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-18 14:09:03 +0000

    link responsive grid

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 function mw_theme_styles(){
   wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+  wp_enqueue_style('responsive_grid_js', get_template_directory_uri() . '/css/responsive-grid.css');
 }
 add_action( 'wp_equeue_scripts', 'mw_theme_styles' );
 
