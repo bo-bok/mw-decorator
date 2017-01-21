d85da70c7e72298ce8c83fe2601afe597eb8cbf4
Jessica Salmon <jessicasalmon05@gmail.com>
2017-01-21 17:26:20 +0000

    tidy header responsive

--- header.php
+++ header.php
@@ -47,15 +47,31 @@
               <nav>
                 <div class="nav-container">
 
+                  <!-- menu icon, displayed on small screens -->
                     <div class="menu-sm-icon">
                       <img class="menu-sm-icon-open" src="<?= get_template_directory_uri()?>/assets/menu-icon-open.svg" alt="<?php echo esc_attr(get_bloginfo( 'name' )) ?>"/>
                     </div>
 
+
+                    <?php
+                      $defaults = array(
+                        'container' => false,
+                        'theme_location' => 'primary-menu', //tells wp where menu lives
+                        'menu_class' => 'nav-large-screen'
+                      );
+
+                      wp_nav_menu( $defaults );
+                    ?>
+
+
+
+                  <!-- modal box menu, displayed when user clicks on menu icon on small screens -->
                   <div class="nav-modal">
 
                     <div class="menu-sm-icon">
                       <img class="menu-sm-icon-close" src="<?= get_template_directory_uri()?>/assets/menu-icon-close.svg" alt="<?php echo esc_attr(get_bloginfo( 'name' )) ?>"/>
                     </div>
+
                     <!-- Displays primary navigation -->
                     <?php
                       $defaults = array(
@@ -67,6 +83,9 @@
                       wp_nav_menu( $defaults );
                     ?>
                   </div>
+
+
+
                 </div>
               </nav>
 
