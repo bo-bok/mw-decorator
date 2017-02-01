ee7e0c7e60ff4822d7de936e503469081892bed9
Jessica Salmon <jessicasalmon05@gmail.com>
2017-02-01 18:24:27 +0000

    set up separate css file & link to site

--- functions.php
+++ functions.php
@@ -23,6 +23,7 @@ function mw_theme_styles(){
   wp_enqueue_style( 'faq-accordion_css', get_template_directory_uri() . '/css/faq-accordion.css' );
   wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
   wp_enqueue_style('responsive_grid_js', get_template_directory_uri() . '/css/responsive-grid.css');
+  wp_enqueue_style('testimonial_slide', get_template_directory_uri() . '/css/testimonial-slide.css');
 }
 add_action( 'wp_enqueue_scripts', 'mw_theme_styles' );
 
