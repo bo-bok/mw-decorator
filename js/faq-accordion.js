cd4a8197904f52eae82648066100c25a8898cb50
Jessica Salmon <jessicasalmon05@gmail.com>
2017-02-01 18:05:55 +0000

    identifies where to add closing styls

--- js/faq-accordion.js
+++ js/faq-accordion.js
@@ -4,20 +4,23 @@
   // Make sure the browser supports what we are about to do.
   if (!document.querySelectorAll || !document.body.classList) return;
 
-  // Using a function helps isolate each accordion from the others
+    // Using a function helps isolate each accordion from the others
   function makeAccordion(accordion) {
     var targets, currentTarget, i;
 
     targets = accordion.querySelectorAll('.faq-question');
     for(i = 0; i < targets.length; i++) {
       targets[i].addEventListener('click', function () {
-        if (currentTarget)
-          currentTarget.classList.remove('expanded');
+        if (currentTarget) {
+            currentTarget.classList.remove('expanded');
+            // effects on close here
+          }
 
         currentTarget = this.parentNode;
         currentTarget.classList.add('expanded');
       }, false);
     }
+
 
     accordion.classList.add('js');
   }
