6a5296626f3713e5115d4607359a5927f2f0d672
Jessica Salmon <jessicasalmon05@gmail.com>
2017-02-08 12:24:55 +0000

    services page laout for ime/ text section

--- page-services.php
+++ page-services.php
@@ -19,6 +19,44 @@ get_header(); ?>
         Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui.
       </div>
 
+      <div class="services-content-container">
+
+        <!-- 1 -->
+        <div class="services-content-section">
+          <div class="services-img">
+            a.1
+          </div>
+
+          <div class="services-text">
+            a.2
+          </div>
+        </div>
+
+        <!-- 2 -->
+        <div class="services-content-section">
+          <div class="services-img">
+            b.1
+          </div>
+
+          <div class="services-text">
+            b.2
+          </div>
+        </div>
+
+        <!-- 3 -->
+        <div class="services-content-section">
+          <div class="services-img">
+            c.1
+          </div>
+
+          <div class="services-text">
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Maecenas faucibus mollis interdum. Curabitur blandit tempus porttitor. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+
+Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.          </div>
+        </div>
+
+      </div>
+
       <div id="testimonial-slide">
         <!-- quotes will display here -->
       </div>
