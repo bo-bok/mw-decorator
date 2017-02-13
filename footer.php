774eb3a882eb117c4697f94c731b859e3182bc21
Jessica Salmon <jessicasalmon05@gmail.com>
2017-02-13 21:59:27 +0000

    split footer into two halves

--- footer.php
+++ footer.php
@@ -8,8 +8,16 @@
   <div class="footer">
     <div class="content-container">
 
-      <div class="footer-copyright">
-      Copyright Michael Ware <?php echo date('Y'); ?>.
+      <div class="row">
+
+        <div class="col-xs-6 footer-copyright">
+          &copy; Michael Ware <?php echo date('Y'); ?>.
+        </div>
+
+        <div class="col-xs-6 footer-email">
+          michaelwarethedecorator@gmail.com
+        </div>
+
       </div>
 
     </div>
@@ -17,6 +25,8 @@
 </footer>
 
   <?php wp_footer(); ?>
+
+
 
 
 <!-- CLOSE -->
