ed4333ca908fe23035845674d63f6a5da4e7f03b
Jessica Salmon <jessicasalmon05@gmail.com>
2017-02-13 18:44:19 +0000

    add wrapper to contact form

--- page-contact.php
+++ page-contact.php
@@ -19,19 +19,19 @@ get_header(); ?>
         Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui.
       </div>
 
+      <div class="contact-form-container">
+        <?php
+        // TO SHOW THE PAGE CONTENTS
+        while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+            <div class="entry-content-page">
+                <?php the_content(); ?> <!-- Page Content -->
+            </div><!-- .entry-content-page -->
 
-      <?php
-      // TO SHOW THE PAGE CONTENTS
-      while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
-          <div class="entry-content-page">
-              <?php the_content(); ?> <!-- Page Content -->
-          </div><!-- .entry-content-page -->
-
-      <?php
-      endwhile; //resetting the page loop
-      wp_reset_query(); //resetting the page query
-      ?>
-
+        <?php
+        endwhile; //resetting the page loop
+        wp_reset_query(); //resetting the page query
+        ?>
+      </div>
 
 
     </div>
