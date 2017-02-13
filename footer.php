51c65d9b981a991d86f815f2ea12a9d284f41eab
Jessica Salmon <jessicasalmon05@gmail.com>
2017-02-13 22:04:28 +0000

    split footer into two rows on small screen

--- footer.php
+++ footer.php
@@ -10,11 +10,11 @@
 
       <div class="row">
 
-        <div class="col-xs-6 footer-copyright">
+        <div class="col-xs-12 col-md-6 footer-copyright">
           &copy; Michael Ware <?php echo date('Y'); ?>.
         </div>
 
-        <div class="col-xs-6 footer-email">
+        <div class="col-xs-12 col-md-6 footer-email">
           michaelwarethedecorator@gmail.com
         </div>
 
